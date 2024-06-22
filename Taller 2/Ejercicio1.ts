eedc943fdc742510532a9b56b5a58d32747d2764
DanCas03 <dacastellanos.22@est.ucab.edu.ve>
2024-06-22 15:37:41 -0400

    Se corrigio errores de gramatica y se terminó la documentacion

--- Taller 2/Ejercicio1.ts
+++ Taller 2/Ejercicio1.ts
@@ -11,46 +11,46 @@ class Box<T> {
             this.box = box
     }
 
-    getvalue(): Optional<T> {
+    getValue(): Optional<T> { // setters y getters
         return this.value;
     }
 
-    setvalue(value: Optional<T>): void {
+    setValue(value: Optional<T>): void {
         this.value = value;
     }
 
-    getbox(): Box<T> {
+    getBox(): Box<T> {
         return this.box;
     }
 
-    setbox(box: Box<T>): void {
+    setBox(box: Box<T>): void {
         this.box = box;
     }
 
-    map<V>(f: (x: T) => V): Either<Error, Box<V>>{
+    map<V>(f: (x: T) => V): Either<Error, Box<V>>{ //Devuelve un either con el error o el nuevo Box
         if (!this.value.hasValue()) {
-            return Either.makeLeft(new Error("Existen cajas vacias"));
+            return Either.makeLeft(new Error("Existen cajas vacias")); //Si una de las cajas esta vacia direcatmente devuelve un error
         }else{
             let tryValue: V;
             try{
                 tryValue = f(this.value.getValue());
             }catch(e){
-                return Either.makeLeft(new Error("Error en la funcion de mapeo"));
+                return Either.makeLeft(new Error("Error en la funcion de mapeo"));//Si hay un error en la funcion de mapeo devuelve un error
             }
             let newValue : Optional<V>= new Optional(tryValue);
             let newBox = new Box(newValue);
 
-            if(this.box){
+            if(this.box){ //Si todo va bien, se mapea la caja interna (si existe)
                 let siguiente: Either<Error, Box<V>>;
                 siguiente = this.box.map(f);
-                if(siguiente.isLeft()){
-                    return Either.makeLeft(siguiente.getLeft());
+                if(siguiente.isLeft()){ //Verifica si hay un error en una de las cajas internas
+                    return Either.makeLeft(siguiente.getLeft());//Si hay un error devuelve el primer error  que encuentre
                 }else{
-                    newBox.setbox(siguiente.getRight());
+                    newBox.setBox(siguiente.getRight()); //Si no hay errores, se setea la caja interna
                 }
             }
 
-            return Either.makeRight(newBox);
+            return Either.makeRight(newBox); //Si todo va bien, devuelve el nuevo Box
         }
     }
 }   
